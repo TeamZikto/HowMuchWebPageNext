b447101ecfe177ba849155c3c00594faf3fb6bbb
= <=>
2020-09-04 14:32:03 +0900

    api base url change

--- config/const.js
+++ config/const.js
@@ -1,5 +1,5 @@
 export default {
-    HOST: `http://localhost:3001`
-    // HOST: "https://howmuch.zikto.com" // 얼마야 release
+    // HOST: `http://localhost:3001`
+    HOST: "https://howmuch.zikto.com" // 얼마야 release
     // HOST: "http://dev.zikto.com:3001" // 얼마야 Dev
 };
